b5177e8523f73875f6c83c0c89833ac942f8179d
mghardy-svg <mghardy@ucdavis.edu>
2026-02-19 11:38:52 -0800

    Update page.tsx

--- src/app/propositions/page.tsx
+++ src/app/propositions/page.tsx
@@ -54,7 +54,10 @@ export default function PropositionsPage() {
 
         if (selectedYear === 'all') {
           // Fetch from all years
-          const yearsToFetch = ['2026', '2025', '2024', '2022', '2021', '2020', '2018', '2016'];
+          const yearsToFetch = ['2026', '2025', '2024', '2023', '2022', '2021', '2020','2019', '2018',
+                                '2017','2016','2015','2014','2013','2012','2011','2010', '2009', '2008', '2007',
+                               '2006', '2005', '2004', '2003', '2002', '2001', '2000', '1999', '1998',
+                               '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990'];
           const fetchPromises = yearsToFetch.map(async (year) => {
             const response = await fetch(`/api/propositions?year=${year}`);
             const data: ApiResponse<Proposition[]> = await response.json();
